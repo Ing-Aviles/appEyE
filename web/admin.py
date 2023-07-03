0b97e20881d7a861bb4cd258de2b4665ef4e107c
Ing-Aviles <134124680+Ing-Aviles@users.noreply.github.com>
2023-07-02 20:36:48 -0600

    Admin t1 creado

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import TipoEmpresa,Empresa
+
+admin.site.register(TipoEmpresa)
+#admin.site.register(Empresa)
+
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ('nombre_Empresa','descripcion','imagen','fecha_registro','cupos')
+    list_editable = ('cupos',)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class TipoEmpresa(models.Model):
+    carrera_Empresa = models.CharField(max_length=30)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.carrera_Empresa
+    
+class Empresa(models.Model):
+    tipo_Empresa = models.ForeignKey(TipoEmpresa, on_delete=models.RESTRICT)
+    nombre_Empresa = models.CharField(max_length=100)
+    descripcion = models.TextField(null=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    imagen = models.ImageField(upload_to='empresa', blank=True)
+    cupos = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return  self.nombre_Empresa
+    
+
+    
+

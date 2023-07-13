853eb5c4d76938488f4784d719c6d4990e900064
Ing-Aviles <134124680+Ing-Aviles@users.noreply.github.com>
2023-07-12 18:40:04 -0600

    Catalago p2

--- web/models.py
+++ web/models.py
@@ -11,7 +11,7 @@ class TipoEmpresa(models.Model):
 class Empresa(models.Model):
     tipo_Empresa = models.ForeignKey(TipoEmpresa, on_delete=models.RESTRICT)
     nombre_Empresa = models.CharField(max_length=100)
-    descripcion = models.TextField(null=True)
+    descripcion = models.CharField(max_length= 100, null=True)
     fecha_registro = models.DateTimeField(auto_now_add=True)
     imagen = models.ImageField(upload_to='empresa', blank=True)
     cupos = models.IntegerField(default=1)
--- web/static/js/catalogoEmpresa.js
+++ web/static/js/catalogoEmpresa.js
@@ -45,3 +45,136 @@
       body.classList.remove("scroll-background");
     }
   });
+
+  $(document).ready(function() {
+    // Agregar evento de clic al botón
+    $("#sidebar-toggle").click(function() {
+      // Alternar la clase "active" en la barra lateral
+      $(".sidebar").toggleClass("active");
+    });
+  });
+
+  const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registroLink = document.querySelector('.registro-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+
+const iconClose = document.querySelector('.icon-close');
+const iconClosed = document.querySelector('.btn-close');
+
+const contenedor = document.querySelector('.contenedor');
+const empLink = document.querySelector('.emp-link');
+const empresaLink = document.querySelector('.empresa-link');
+const btnRegistrar = document.querySelector('.btnRegistrar-popup');
+const boton = document.querySelector('.boton');
+
+const vista = document.querySelector('.vista');
+const btnLoginPopup = document.querySelector('.btnLogin-popup');
+const btnRegistrarPopup = document.querySelector('.btnRegistrar-popup');
+
+const btnCerrarLogin = document.querySelector('.wrapper .icon-close');
+const btnCerrarRegistro = document.querySelector('.wrapper .icon-close');
+
+const BtnCerrarLogin = document.querySelector('.contenedor .btn-close');
+const BtnCerrarRegistro = document.querySelector('.contenedor .btn-close');
+
+/*BtnPopup.addEventListener('click', () => {
+  vista.classList.remove('desactivar');
+});
+Btnpopup.addEventListener('click', () => {
+  vista.classList.remove('desactivar-popup');
+})*/
+
+//Eventos para que desaparezca el div vista al clikear empresa o estudinte
+btnLoginPopup.addEventListener('click', () => {
+  vista.style.display = 'none';
+});
+btnRegistrarPopup.addEventListener('click', () => {
+  vista.style.display = 'none';
+});
+
+registroLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
+
+
+btnRegistrar.addEventListener('click', () => {
+  contenedor.classList.add('activo-popup');
+});
+iconClosed.addEventListener('click', () => {
+  contenedor.classList.remove('activo-popup');
+});
+empresaLink.addEventListener('click', () => {
+  contenedor.classList.add('activo');
+});
+empLink.addEventListener('click', () => {
+  contenedor.classList.remove('activo');
+});
+
+btnCerrarLogin.addEventListener('click', () => {
+  vista.style.display = 'block';
+});
+btnCerrarRegistro.addEventListener('click', () => {
+  vista.style.display = 'block';
+});
+
+BtnCerrarLogin.addEventListener('click', () => {
+  vista.style.display = 'block';
+});
+BtnCerrarRegistro.addEventListener('click', () => {
+  vista.style.display = 'block';
+});
+
+const yearSelect = document.getElementById("year");
+
+// obtener el año actual
+const currentYear = new Date().getFullYear();
+
+// generar opciones de años y agregarlas al elemento select
+for (let year = currentYear; year >= 2010; year--) {
+  const option = document.createElement("option");
+  option.text = year;
+  option.value = year;
+  yearSelect.add(option);
+}
+
+function validarFormulario() {
+  // obtener los campos del formulario del primer registro
+  const programa = document.getElementsByName("programa")[0];
+  const proceso = document.getElementsByName("proceso")[0];
+  const periodo = document.getElementsByName("periodo")[0];
+  const ano = document.getElementsByName("año")[0];
+  const razon = document.getElementsByName("razon")[0];
+  const nombre = document.getElementsByName("nombre")[0];
+  const cargo = document.getElementsByName("cargo")[0];
+
+  // validar que los campos no estén vacíos
+  if (nombre.value === "" || programa.value === "" || proceso.value === "" || periodo.value === "" || ano.value === "" || razon.value === "" || cargo.value === "") {
+    alert("Por favor complete todos los campos del registro 1.");
+  } else {
+    // si los campos están completos, enviar al usuario al formulario del segundo registro
+    document.getElementById("form-box.direccion").submit();
+  }
+}
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+}
